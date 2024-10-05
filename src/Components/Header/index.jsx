f0d7b2dab896143f928b63dbb5e5017b01267206
Abdur Rahman <abdulrehmanmazhar02@gmail.com>
2024-10-05 17:18:18 +0500

    navbar done

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -4,6 +4,7 @@ import logo from "../../assets/logo.png";
 import Button from "@mui/material/Button";
 import { LuUser2 } from "react-icons/lu";
 import { IoBagOutline } from "react-icons/io5";
+import { FaAngleDown } from "react-icons/fa";
 
 import { Link } from "react-router-dom";
 import CountryDropdown from "../CountryDropdown";
@@ -24,7 +25,7 @@ function Header() {
         <header className="header">
           <div className="container">
             <div className="row">
-              <div className="logoWrapper d-flex align-items-center col-sm-2">
+              <div className="logoWrapper d-flex align-items-center justify-content-center col-sm-2">
                 <Link to={"/"}>
                   {" "}
                   <img src={logo} alt="logo" />{" "}
@@ -45,8 +46,7 @@ function Header() {
                     <span className="price">$3.29</span>
                     <div className="position-relative ml-2">
                     <Button className="circle">
-                    <IoBagOutline />
-
+                      <IoBagOutline />
                     </Button>
                     <span className="count d-flex align-items-center justify-content-center">1</span>
                     </div>
@@ -56,6 +56,33 @@ function Header() {
             </div>
           </div>
         </header>
+
+        <nav>
+          <div className="container">
+            <div className="row">
+              <div className="navPart1 col-sm-3">
+                <Button className="allCatTab">
+                  <span className="text">All Categories</span>
+                  <span>
+
+                <FaAngleDown />
+                  </span>
+                </Button>
+              </div>
+              <div className="navPart2 col-sm-9 d-flex align-items-center">
+                <ul className="list list-inline w-100">
+                  <li className="list-inline-item"> <Link to={"/"}>Home</Link> </li>
+                  <li className="list-inline-item"> <Link to={"/"}>Best Selling</Link> </li>
+                  <li className="list-inline-item"> <Link to={"/"}>Premium</Link> </li>
+                  <li className="list-inline-item"> <Link to={"/"}>Hot Sales</Link> </li>
+                  <li className="list-inline-item"> <Link to={"/"}>New In</Link> </li>
+                  <li className="list-inline-item"> <Link to={"/"}>Contact Us</Link> </li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
     </>
   );
