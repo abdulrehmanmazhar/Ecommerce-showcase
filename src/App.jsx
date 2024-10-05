ead957e4e506dae158f2c25b677aa25285d59cc3
Abdur Rahman <abdulrehmanmazhar02@gmail.com>
2024-10-05 11:03:19 +0500

    Header Started

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import Header from './Components/Header'
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='headerWrapper'>
+    <div className='top-strip bg-bluer'>
+      <div className='container'>
+        <p className='mb-0 mt-0 text-center'>Lorem ipsum dolor sit amet consectetur <b>adipisicing</b>  elit. Eligendi, sapiente?</p>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
