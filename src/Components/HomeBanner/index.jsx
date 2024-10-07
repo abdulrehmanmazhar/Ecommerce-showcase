fa6d82349bb7eaae37f1c2b390258cb03148b069
Abdur Rahman <abdulrehmanmazhar02@gmail.com>
2024-10-07 13:37:59 +0500

    banner fixed and ProductItem done with carousel

--- src/Components/HomeBanner/index.jsx
+++ src/Components/HomeBanner/index.jsx
@@ -1,13 +1,77 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+// import { GrPrevious, GrNext } from "react-icons/gr";
+import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
 
-function HomeBanner() {
+
+function HomeBanner({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [record, setRecord] = useState([]);
+  const parentRef = useRef(null);
+  const intervalRef = useRef(null);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  let indFunc = function (record) {
+    return () => {
+      const currentIndicator = parentRef.current.childNodes[currentIndex];
+
+      // Assign the "now" class to the current indicator
+      currentIndicator.id = 'now';
+
+      // Update the record array
+      let arrayState = [...record, currentIndicator];
+      setRecord(arrayState);
+      let vanish = record[record.length - 1];
+      if (vanish) {
+        vanish.id = "";
+      }
+    };
+  };
+  let cloIndFunc = indFunc(record);
+  useEffect(() => {
+    cloIndFunc();
+  }, [currentIndex]);
+
+  useEffect(() => {
+    // Set up the interval
+    intervalRef.current = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    // Clear the interval when the component unmounts
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
   return (
-    <div className='homeBannerSection'>
-      <div className='row'>
-        <div className='col-md-3'></div>
-        <div className='col-md-7'></div>
+    <div className='carousel'>
+      <div className='image'>
+        <img src={images[currentIndex]} alt="" />
       </div>
-    </div>  )
+      <div className="controls">
+        <div className='buttons'>
+          <FaAngleLeft
+          className='prev' onClick={prevSlide} />
+          <FaAngleRight
+          className='next' onClick={nextSlide} />
+        </div>
+        <div className='indbtns' ref={parentRef}>
+          {images.map((image, index) => (
+            <div key={index} className={`ind `} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HomeBanner
+export default HomeBanner;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
 import HomeBanner from '../../Components/HomeBanner'
+import banner1 from '../../assets/banner1.jpg'
+import Button from "@mui/material/Button";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import slideBanner1 from '../../assets/slideBanner1.jpg'
+import slideBanner2 from '../../assets/slideBanner2.jpg'
+import ProductItem from '../../Components/ProductItem';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 function Home() {
   return (
     <>
-    <HomeBanner></HomeBanner>
+    <HomeBanner images={
+      [slideBanner1,slideBanner2]
+      // [
+      //       "https://www.searchenginejournal.com/wp-content/uploads/2017/02/Banner-with-SEO-Expert-1024x539.jpg",
+      //       'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'
+      //     ]
+          }></HomeBanner>
+          <section className='homeProducts'>
+            <div className="conatiner">
+              <div className="row">
+                <div className="col-md-3">
+                  <img className='banner cursor' src={banner1} alt="" />
+                </div>
+                <div className="col-md-9">
+                  <div className="d-flex align-items-center">
+                    <div className="info w-75">
+                      <h3 className='bannerHeading'>BEST SELLING</h3>
+                      <p>Here are our proud most popular items among our customers.</p>
+                    </div>
+                    <Button>View All  <FaArrowRightLong />
+                    </Button>
+                  </div>
+                  <div className="product_row w-100">
+                  <Swiper
+        slidesPerView={3}
+        spaceBetween={0}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <ProductItem></ProductItem>
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductItem></ProductItem>
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductItem></ProductItem>
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductItem></ProductItem>
+        </SwiperSlide>
+
+      </Swiper>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
     </>
   )
 }
